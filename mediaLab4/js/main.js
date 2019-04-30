2e4cccefacd45640e3e8bf3424fb1ebb8ad818e6
pauliusaka <49921112+pauliusaka@users.noreply.github.com>
2019-04-30 10:12:04 +0300

    Create main.js

---
+++ mediaLab4/js/main.js
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+'use strict';
+
+const snapshotButton = document.querySelector('button#snapshot');
+const filterSelect = document.querySelector('select#filter');
+const pomidoras = document.querySelector('option[value=red]');
+const redButton = document.querySelector('button#red');
+
+// Put variables in global scope to make them available to the browser console.
+const video = window.video = document.querySelector('video');
+const canvas = window.canvas = document.querySelector('canvas');
+canvas.width = 480;
+canvas.height = 360;
+
+snapshotButton.onclick = function() {
+  canvas.className = filterSelect.value;
+  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+	
+};
+
+filterSelect.onchange = function() {
+  video.className = filterSelect.value;
+};
+
+const constraints = {
+  audio: false,
+  video: true
+};
+
+function handleSuccess(stream) {
+  window.stream = stream; // make stream available to browser console
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+
+pomidoras.onclick = function() {
+    var ctx = canvas.getContext('2d');
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+    for (var i = 0; i < data.length; i += 4) {
+      //data[i]     = avg; // red
+      data[i + 1] = 0; // green
+      data[i + 2] = 0; // blue
+    }
+    ctx.putImageData(imageData, 0, 0);
+  };
+redButton.onclick = function() {
+    var ctx = canvas.getContext('2d');
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+    for (var i = 0; i < data.length; i += 4) {
+      //data[i]     = avg; // red
+      data[i + 1] = 0; // green
+      data[i + 2] = 0; // blue
+    }
+    ctx.putImageData(imageData, 0, 0);
+  };
